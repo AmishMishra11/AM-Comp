d9efa3432f39f1362a4bb28f4b6b96e40284afcb
AmishMishra11 <amishmishra11@gmail.com>
2022-02-20 21:45:01 +0530

    fix: made it so dark mode dosent change on page reload

--- darkmode.js
+++ darkmode.js
@@ -2,7 +2,26 @@ const darkBtn = document.querySelector(".dark-mode");
 const bodyContent = document.querySelector(".header");
 
 function clickHander() {
-  console.log("here");
-  bodyContent.classList.toggle("dark");
+  const modeClick = localStorage.getItem("colorMode");
+  if (modeClick == "dark") {
+    localStorage.setItem("colorMode", "light");
+    bodyContent.classList.remove("dark");
+  } else if (modeClick == "light") {
+    localStorage.setItem("colorMode", "dark");
+    bodyContent.classList.add("dark");
+  } else {
+    localStorage.setItem("colorMode", "light");
+  }
 }
+
 darkBtn.addEventListener("click", clickHander);
+
+(() => {
+  const mode = localStorage.getItem("colorMode");
+  console.log(mode);
+  if (mode == "dark") {
+    bodyContent.classList.add("dark");
+  } else {
+    bodyContent.classList.remove("dark");
+  }
+})();
